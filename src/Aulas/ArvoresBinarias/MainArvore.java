673f16c141ee25fd179fd71c6eebcf4d653eb393
5220336 <jesse.outside@gmail.com>
2024-04-29 22:02:52 -0300

    Commit Aula 29/04-24

---
+++ src/Aulas/ArvoresBinarias/MainArvore.java
@@ -0,0 +1,11 @@
+package Aulas.ArvoresBinarias;
+
+public class MainArvore {
+    public static void main(String[] args) {
+        ArvoreTAD arv = new ArvoreTAD();
+
+        System.out.println(arv.estaVazia());
+        System.out.println(arv.pesquisa(15));
+    }
+
+}
