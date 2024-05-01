11cdd8cf6f62bd8555b77cc27befa8c596b31b31
Jessé Boeira <jee_boeira@hotmail.com>
2024-05-01 02:21:03 -0300

    Finalizado Exercício Recursividade :tada:

--- src/Aulas/Recursividade/Palindromo.java
+++ src/Aulas/Recursividade/Palindromo.java
@@ -6,15 +6,20 @@ public class Palindromo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String palavra = scan.next();
-        //System.out.println(palindromo("osso"));
+        System.out.println(palindromo(palavra));
     }
 
-    /*public static boolean palindromo(String palavra) {
+    public static boolean palindromo(String palavra) {
         return palindromoRec(palavra, 0, palavra.length()-1);
     }
 
-    public static boolean palindromoRec(String palavra, int i, int f){
-        if ()
-        palindromoRec(palavra, i+1,f-1);
-    }*/
+    public static boolean palindromoRec(String palavra, int i, int f) {
+        if (palavra.charAt(i) != palavra.charAt(f)) {
+            return false;
+        } else if (palavra.charAt(i) >= palavra.charAt(f)) {
+            return true;
+        } else {
+            return palindromoRec(palavra, i + 1, f - 1);
+        }
+    }
 }
