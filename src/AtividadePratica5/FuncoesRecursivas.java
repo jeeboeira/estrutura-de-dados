4e8b418118981992e60793fed3a1a1234ac8e550
Jessé Boeira <jee_boeira@hotmail.com>
2024-05-01 02:51:38 -0300

    Finalizado Exercício Recursividade :tada:

---
+++ src/AtividadePratica5/FuncoesRecursivas.java
@@ -0,0 +1,18 @@
+package AtividadePratica5;
+
+public class FuncoesRecursivas {
+    public static void main(String[] args) {
+        System.out.println(div(1,1));
+        System.out.println(div(1,2));
+        System.out.println(div(5,4));
+    }
+    public static int div(int x, int y) {
+        if (x == y){
+            return 1;
+        } else if (x < y) {
+            return 0;
+        } else {
+            return 1 + div(x-y,y);
+        }
+    }
+}
