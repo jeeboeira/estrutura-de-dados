49955268713f954c3e535f2555adde5c34982c8f
5220336 <jesse.outside@gmail.com>
2024-06-03 22:11:38 -0300

    Programa Completo! :tada:

--- src/Aulas/ArvoresBinarias/MainArvore.java
+++ src/Aulas/ArvoresBinarias/MainArvore.java
@@ -18,7 +18,6 @@ public class MainArvore {
         arv.imprimeArvore();
         System.out.println(arv.estaVazia());
         System.out.println(arv.pesquisa(10));
-        //arv.tamanho();
     }
 
 }
